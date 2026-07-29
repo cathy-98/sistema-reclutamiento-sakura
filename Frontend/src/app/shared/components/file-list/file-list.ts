1f07cf6e3722b53b6c40c5b0989dc73b93466562
cathy-98 <catherine.rebolledo@elitsoft-chile.com>
2026-07-29 16:14:37 -0400

    Mejora en la carga de cvs

--- Frontend/src/app/shared/components/file-list/file-list.ts
+++ Frontend/src/app/shared/components/file-list/file-list.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Button } from '../button/button';
+import { IconButton } from '../icon-button/icon-button';
 
 @Component({
   selector: 'app-file-list',
-  imports: [CommonModule, Button],
+  imports: [CommonModule, Button, IconButton],
   templateUrl: './file-list.html',
   styleUrl: './file-list.scss',
 })
@@ -14,7 +15,15 @@ export class FileList {
   @Output() clear = new EventEmitter<void>();
   @Output() remove = new EventEmitter<File>();
 
+  get totalSizeMb() {
+    return this.files.reduce((total, file) => total + this.fileSizeMb(file), 0);
+  }
+
   fileSizeMb(file: File) {
     return file.size / 1024 / 1024;
+  }
+
+  trackFile(_index: number, file: File) {
+    return `${file.name}-${file.size}-${file.lastModified}`;
   }
 }
